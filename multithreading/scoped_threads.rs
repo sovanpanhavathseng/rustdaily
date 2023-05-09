c57ede82723efc3a1f427fd0eb4ea6f0a20365e2
Seng Keat <corodostudio@gmail.com>
2023-05-10 03:08:32 +0700

    Update scoped_threads.rs

--- multithreading/scoped_threads.rs
+++ multithreading/scoped_threads.rs
@@ -1,7 +1,19 @@
 
+extern crate crossbeam;
 
 // 
 fn main(){
+
+// create a crate vector from 0-1000 - then for each of them for the crossbeam scope
   let all_nums: Vec<_> = (0..1_000_u64).into_iter().collect();
     let mut results = Vec::new();
+    
+    crossbeam::scope(|scope| {
+    for num in &all_nums {
+    results.push(scope.spawn(move || num * num + num * 5 + 2500));
+    }
+});
+    
+    let final_result: u64d = results.into_iter().map(|res| res.join()).sum();
+    println!("Final result:{}", final_result);
 }
